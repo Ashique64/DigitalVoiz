0c23dbdfd9fd04370ea3ebd8301aa1fb925effb3
Ashique <ashique9496@gmail.com>
2025-04-12 11:12:33 +0530

    Cursor effect implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
-import AdvancedCursorEffect from "./components/AdvancedCursorEffect";
+import AnimatedCursor from "react-animated-cursor";
 
 const App = () => {
     return (
         <>
-            
-            {/* <AdvancedCursorEffect/> */}
+            <AnimatedCursor innerSize={8} outerSize={35} color="0, 255, 0" outerAlpha={0.2} innerScale={1} outerScale={2} />
             <Home />
         </>
     );
